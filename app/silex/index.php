b252499e71b4cf80042924382f5c9fa555bb22f4
Carlos Buenosvinos <hi@carlos.io>
2014-02-23 20:02:48 +0100

    Add Silex and reorganize dependencies

---
+++ app/silex/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use Etpa\Infraestructure\Persistence\Doctrine\EntityManagerFactory;
+use Etpa\Tests\Infraestructure\Persistence\StoryRepository\EmptyStoryRepository;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app['em'] = function() {
+    return (new EntityManagerFactory())->build();
+};
+
+$app->get('/stories', function () use ($app) {
+    $storyRepository = $app['em']->getRepository('Etpa\Domain\Story');
+    $storyRepository = new EmptyStoryRepository();
+
+    $usecase = new \Etpa\UseCases\Story\ViewStoriesUseCase($storyRepository);
+    $response = $usecase->viewStories(new \Etpa\UseCases\Story\ViewPageRequest());
+
+    return $app->escape(print_r($response->stories, 1));
+});
+
+$app->get('/page/{id}', function ($id) use ($app) {
+    $pageRepository = $app['em']->getRepository('Etpa\Domain\Story');
+    $storyRepository = new EmptyStoryRepository();
+
+    $usecase = new \Etpa\UseCases\Page\ViewPageUseCase($pageRepository);
+    $response = $usecase->viewStories(new \Etpa\UseCases\Story\ViewPageRequest());
+
+    return $app->escape(print_r($response->stories, 1));
+});
+
+$app->get('/story/add', function () use ($app) {
+    $usecase = new \Etpa\UseCases\Story\ViewStoriesUseCase($app['em']->getRepository('Etpa\Domain\Story'));
+    $response = $usecase->viewStories(new \Etpa\UseCases\Story\ViewPageRequest());
+
+    return $app->escape(print_r($response->stories, 1));
+});
+
+$app->run();
