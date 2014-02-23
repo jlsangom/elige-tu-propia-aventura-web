0b85d6275b187957af728156d96472bef28f244b
Carlos Buenosvinos <hi@carlos.io>
2014-02-24 00:10:17 +0100

    Add some Twig

--- app/silex/index.php
+++ app/silex/index.php
@@ -1,7 +1,11 @@
 <?php
 
 use Etpa\Infraestructure\Persistence\Doctrine\EntityManagerFactory;
-use Etpa\Tests\Infraestructure\Persistence\StoryRepository\EmptyStoryRepository;
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
@@ -11,22 +15,29 @@ $app['em'] = function() {
     return (new EntityManagerFactory())->build();
 };
 
-$app->get('/stories', function () use ($app) {
-    $storyRepository = $app['em']->getRepository('Etpa\Domain\Story');
-    $storyRepository = new EmptyStoryRepository();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 
+$app->get('/stories', function () use ($app) {
+    $request = new \Etpa\UseCases\Story\ViewStoriesRequest();
+
+    $storyRepository = $app['em']->getRepository('Etpa\Domain\Story');
     $usecase = new \Etpa\UseCases\Story\ViewStoriesUseCase($storyRepository);
-    $response = $usecase->viewStories(new \Etpa\UseCases\Story\ViewStoriesRequest());
+    $response = $usecase->viewStories($request);
 
     return $app->escape(print_r($response->stories, 1));
 });
 
 $app->get('/page/{id}', function ($id) use ($app) {
+    $request = new \Etpa\UseCases\Page\ViewPageRequest();
+    $request->pageId = $id;
+
     $pageRepository = $app['em']->getRepository('Etpa\Domain\Page');
     $usecase = new \Etpa\UseCases\Page\ViewPageUseCase($pageRepository);
-    $response = $usecase->viewPage(new \Etpa\UseCases\Page\ViewPageRequest());
+    $response = $usecase->viewPage($request);
 
-    return $app->escape(print_r($response->stories, 1));
+    return $app['twig']->render('view-page.html.twig', ['response' => $response]);
 });
 
 $app->get('/story/add', function () use ($app) {
@@ -34,6 +45,22 @@ $app->get('/story/add', function () use ($app) {
     $response = $usecase->viewStories(new \Etpa\UseCases\Story\ViewPageRequest());
 
     return $app->escape(print_r($response->stories, 1));
+});
+
+$app->get('/reset', function () use ($app) {
+    $storyRepository = $app['em']->getRepository('Etpa\Domain\Story');
+    $story = new \Etpa\Domain\Story();
+    $story->setTitle('El laberinto');
+    $story->setDescription('Un laberinto sin fin del que tendrás que salir, si puedes.');
+    $storyRepository->persist($story);
+
+    $pageRepository = $app['em']->getRepository('Etpa\Domain\Page');
+    $page = new \Etpa\Domain\Page();
+    $page->setTitle('Inicio y fin');
+    $page->setDescription('Es una historia que tiene inicio y fin.');
+    $pageRepository->persist($page);
+
+    return $app->escape('Done!');
 });
 
 $app->run();
